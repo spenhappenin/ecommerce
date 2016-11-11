e8f04612b16f2d78bfd820152e84d9fcda906107
Josh Terry <joshterry55@gmail.com>
2016-11-11 16:50:03 -0700

    added routes for cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ root 'store#index'
 get '/about', to: 'shared#about', as: 'about'
 get '/cart', to: 'cart#index', as: 'cart'
 
+resources :cart
+
 resources :stores do
   resources :departments
 end
 
-resources :departments do
+resources :departments, except: [:index, :show, :edit, :new, :create, :update, :destroy]do
   resources :products
 end
 end
