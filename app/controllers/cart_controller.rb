114525ef4ec409b24f4f773a8ddd288247dfd235
Josh Terry <joshterry55@gmail.com>
2016-11-14 14:18:46 -0700

    add to cart button

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,41 +1,15 @@
 class CartController < ApplicationController
-  before_action :set_cart, except: [:index, :new, :create]
-  def index
-    @carts = Carts.all
-  end
+  before_action :set_cart
 
   def show
   end
 
-  def new
-    @cart = Cart.new
-  end
-
-  def create
-    @cart = Cart.new(cart_params)
-    if @cart.save
-      redirect_to cart_path(@cart)
-    else
-      render :new
-    end
-  end
-
-  def edit
-
-  end
-
-  def update
-  end
-
-  def destroy
+  def add_product
+    @cart.products << Product.find(params[:product_id])
   end
 
   private
-  def cart_params
-    params.require(:cart).permit
-  end
-
-  def set_cart
-    @cart = Cart.find(params[:id])
-  end
+    def set_cart
+      @cart = Cart.first
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
+  root 'shared#home'
 
-root 'shared#home'
+  get '/about', to: 'shared#about', as: 'about'
+  get '/cart', to: 'cart#index', as: 'cart'
+  get '/contact', to: 'shared#contact', as: 'contact'
 
-get '/about', to: 'shared#about', as: 'about'
-get '/cart', to: 'cart#index', as: 'cart'
-get '/contact', to: 'shared#contact', as: 'contact'
+  # Custom Routes To Add Songs To A Billboard
+  post 'add_cart_product/:product_id', to: 'cart#add_product', as: 'add_cart_product'
 
-# Custom Routes To Add Songs To A Billboard
-get 'new_cart_product/:id', to: 'cart#new_product', as: 'new_cart_product'
-post 'add_cart_product/:id/:product_id', to: 'carts#add_product', as: 'add_cart_product'
+  resources :stores do
+    resources :departments
+  end
 
-resources :cart
-
-resources :stores do
-  resources :departments
-end
-
-resources :departments, except: [:index, :show, :edit, :new, :create, :update, :destroy]do
-  resources :products
-end
+  resources :departments, except: [:index, :show, :edit, :new, :create, :update, :destroy]do
+    resources :products
+  end
 end
