9235ee28529c46e6bff5e57d5ec9cc06d506c567
William Winger <Billy_thekid55@hotmail.com>
2016-11-14 15:11:00 -0700

    added collapsible department menu

--- db/schema.rb
+++ db/schema.rb
@@ -53,13 +53,6 @@ ActiveRecord::Schema.define(version: 20161114212126) do
     t.string   "color"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "departments", "stores"
   add_foreign_key "products", "departments"
 end
