46521240f38cf766788ebcbfa2ccc74dbd787765
William Winger <Billy_thekid55@hotmail.com>
2016-11-14 22:02:13 -0700

    user stores and all stores working

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/cart', to: 'cart#show', as: 'cart'
   get '/contact', to: 'shared#contact', as: 'contact'
 
+  get 'all_stores', to: 'stores#all_stores'
+
   # Custom Routes To Add Songs To A Billboard
   post 'add_cart_product/:product_id', to: 'cart#add_product', as: 'add_cart_product'
   delete 'remove_cart_product/:product_id', to: 'cart#remove_product', as: 'remove_cart_product'
@@ -20,4 +22,3 @@ Rails.application.routes.draw do
     resources :products
   end
 end
-
